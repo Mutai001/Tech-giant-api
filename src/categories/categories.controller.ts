334d7bddf2364538d7aa4365bf641c419093b4ca
Mutai001 <omusekim@gmail.com>
2025-05-06 11:26:36 +0300

    categories controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,14 +1,18 @@
-//users.controller.ts
 import { Context } from "hono";
-import { usersService, getuserservice, createuserservice, updateuserservice, deleteuserservice } from "./users.service";
-import * as bcrypt from "bcrypt";
+import { 
+  categoriesService, 
+  getCategoryService, 
+  createCategoryService, 
+  updateCategoryService, 
+  deleteCategoryService 
+} from "./categories.service";
 
-export const listUsers = async (c: Context) => {
+export const listCategories = async (c: Context) => {
   try {
     const limit = Number(c.req.query("limit"));
-    const data = await usersService(limit);
+    const data = await categoriesService(limit);
     if (data == null || data.length === 0) {
-      return c.text("User not found", 404);
+      return c.text("No categories found", 404);
     }
     return c.json(data, 200);
   } catch (error: any) {
@@ -16,63 +20,59 @@ export const listUsers = async (c: Context) => {
   }
 };
 
-export const getUser = async (c: Context) => {
+export const getCategory = async (c: Context) => {
   const id = parseInt(c.req.param("id"));
   if (isNaN(id)) return c.text("Invalid ID", 400);
 
-  const user = await getuserservice(id);
-  if (user === undefined) {
-    return c.text("User not found", 404);
+  const category = await getCategoryService(id);
+  if (category === undefined) {
+    return c.text("Category not found", 404);
   }
-  return c.json(user, 200);
+  return c.json(category, 200);
 };
 
-export const createUser = async (c: Context) => {
+export const createCategory = async (c: Context) => {
   try {
-    const user = await c.req.json();
-    const password = user.password;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user.password = hashedPassword;
-    const createduser = await createuserservice(user);
+    const category = await c.req.json();
+    const createdCategory = await createCategoryService(category);
 
-    if (!createduser) return c.text("User not created", 404);
-    return c.json({ msg: createduser }, 201);
+    if (!createdCategory) return c.text("Category not created", 400);
+    return c.json({ msg: createdCategory }, 201);
   } catch (error: any) {
     return c.json({ error: error?.message }, 400);
   }
 };
 
-export const updateUser = async (c: Context) => {
+export const updateCategory = async (c: Context) => {
   const id = parseInt(c.req.param("id"));
   if (isNaN(id)) return c.text("Invalid ID", 400);
 
-  const user = await c.req.json();
+  const category = await c.req.json();
   try {
-    const searcheduser = await getuserservice(id);
-    if (searcheduser === undefined) return c.text("User not found", 404);
+    const searchedCategory = await getCategoryService(id);
+    if (searchedCategory === undefined) return c.text("Category not found", 404);
 
-    const res = await updateuserservice(id, user);
+    const res = await updateCategoryService(id, category);
+    if (!res) return c.text("Category not updated", 400);
 
-    if (!res) return c.text("User not updated", 404);
-
-    return c.json({ msg: res }, 201);
+    return c.json({ msg: res }, 200);
   } catch (error: any) {
     return c.json({ error: error?.message }, 400);
   }
 };
 
-export const deleteUser = async (c: Context) => {
+export const deleteCategory = async (c: Context) => {
   const id = Number(c.req.param("id"));
   if (isNaN(id)) return c.text("Invalid ID", 400);
 
   try {
-    const user = await getuserservice(id);
-    if (user === undefined) return c.text("User not found", 404);
+    const category = await getCategoryService(id);
+    if (category === undefined) return c.text("Category not found", 404);
 
-    const res = await deleteuserservice(id);
-    if (!res) return c.text("User not deleted", 404);
+    const res = await deleteCategoryService(id);
+    if (!res) return c.text("Category not deleted", 400);
 
-    return c.json({ msg: res }, 201);
+    return c.json({ msg: res }, 200);
   } catch (error: any) {
     return c.json({ error: error?.message }, 400);
   }
