976620661106e3421491891348f5fbda1450aaf0
Mutai001 <omusekim@gmail.com>
2025-05-06 10:23:09 +0300

    rourer

---
+++ src/admin-authentication/admin.router.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator';
+import { registerAdmin } from './admin.controller';
+import { registerAdminSchema } from './admin.validator';
+
+export const adminRouter = new Hono();
+
+adminRouter.post('/register', 
+    zValidator('json', registerAdminSchema, (result, c) => {
+        if (!result.success) {
+            return c.json(result.error.issues, 400);
+        }
+    }), 
+    registerAdmin
+);
