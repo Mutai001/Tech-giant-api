bcf151952a030bf231a49eced0ee8439dbf6cb48
Mutai001 <omusekim@gmail.com>
2025-05-06 12:48:42 +0300

    product router

---
+++ src/product/product.router.ts
@@ -0,0 +1,44 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  addMedia,
+  removeMedia
+} from "./product.controller";
+import { productSchema, productMediaSchema } from "./product.validator";
+import { adminRoleAuth } from "../middleware/bearAuth";
+
+export const productRouter = new Hono();
+
+// Public routes
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProduct);
+
+// Protected routes
+productRouter.use("/*", adminRoleAuth);
+
+productRouter.post(
+  "/",
+  zValidator("json", productSchema, (result, c) => {
+    if (!result.success) return c.json(result.error, 400);
+  }),
+  createProduct
+);
+
+productRouter.put("/:id", updateProduct);
+productRouter.delete("/:id", deleteProduct);
+
+// Media routes
+productRouter.post(
+  "/media",
+  zValidator("json", productMediaSchema, (result, c) => {
+    if (!result.success) return c.json(result.error, 400);
+  }),
+  addMedia
+);
+
+productRouter.delete("/media/:mediaId/product/:productId", removeMedia);
