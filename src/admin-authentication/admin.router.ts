4f64fc4134e0bdd51453f95c68982f02158d7030
Mutai001 <omusekim@gmail.com>
2025-05-06 10:32:35 +0300

    admin router with login

--- src/admin-authentication/admin.router.ts
+++ src/admin-authentication/admin.router.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono';
 import { zValidator } from '@hono/zod-validator';
-import { registerAdmin } from './admin.controller';
-import { registerAdminSchema } from './validator';
+import { registerAdmin, loginAdmin } from './admin.controller';
+import { registerAdminSchema, loginAdminSchema } from './validator';
 
 export const adminRouter = new Hono();
 
+// Admin Registration
 adminRouter.post('/register', 
     zValidator('json', registerAdminSchema, (result, c) => {
         if (!result.success) {
@@ -12,4 +13,14 @@ adminRouter.post('/register',
         }
     }), 
     registerAdmin
+);
+
+// Admin Login
+adminRouter.post('/login', 
+    zValidator('json', loginAdminSchema, (result, c) => {
+        if (!result.success) {
+            return c.json(result.error.issues, 400);
+        }
+    }), 
+    loginAdmin
 );
