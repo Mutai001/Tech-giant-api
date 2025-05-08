a5e20595e6f74ade3a8d3a607b2e63b2042c47b2
Mutai001 <omusekim@gmail.com>
2025-05-08 22:34:35 +0300

    orders router

---
+++ src/orders/orders.router.ts
@@ -0,0 +1,38 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import {
+  listUserOrders,
+  getOrder,
+  createNewOrder,
+  updateOrderStatus,
+  cancelUserOrder,
+  listOrdersByStatus
+} from "./orders.controller";
+import { orderSchema, orderUpdateSchema } from "./orders.validator";
+
+const orderRouter = new Hono();
+
+// User-specific routes
+orderRouter.get("/user/:userId{[0-9]+}", listUserOrders);
+
+// Status-based routes
+orderRouter.get("/status/:status", listOrdersByStatus);
+
+// Order operations
+orderRouter.post(
+  "/",
+  zValidator("json", orderSchema),
+  createNewOrder
+);
+
+orderRouter.get("/:id{[0-9]+}", getOrder);
+
+orderRouter.patch(
+  "/:id{[0-9]+}",
+  zValidator("json", orderUpdateSchema),
+  updateOrderStatus
+);
+
+orderRouter.delete("/:id{[0-9]+}/cancel", cancelUserOrder);
+
+export default orderRouter;
