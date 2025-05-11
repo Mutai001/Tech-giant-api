2d9d43f361710c74e10c07eef2aa4c74286cc8dc
Mutai001 <omusekim@gmail.com>
2025-05-12 01:24:54 +0300

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,8 @@ app.use(
   "*",
   cors({
     origin: [
-      "http://localhost:5173", 
+      "http://localhost:5173",
+      "https://tech-giants.vercel.app" 
         // deployed frontend
     ],
     allowHeaders: ["Content-Type", "Authorization"],
